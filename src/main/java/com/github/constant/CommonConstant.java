fca512d54d24b7a22fba9b627566d95bfcec8167
Khanh Nguyen <pb20.khanhnh@techcombank.com.vn>
2025-03-14 10:19:21 +0700

    some commits

---
+++ src/main/java/com/github/constant/CommonConstant.java
@@ -0,0 +1,9 @@
+package com.github.constant;
+
+public class CommonConstant {
+    private CommonConstant() {
+    }
+
+    public static final int TOTAL_SIZE = 50_000;
+    public static final int CHUNK_SIZE = 5_000;
+}
