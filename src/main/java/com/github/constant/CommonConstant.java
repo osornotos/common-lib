871bb4152d03a6edeb3e997f60f24bd3fdd7fc78
Part time lifter <khanhnh18091994@gmail.com>
2025-03-14 04:16:39 +0000

    Maven artifacts for 1.0-SNAPSHOT

--- src/main/java/com/github/constant/CommonConstant.java
+++
@@ -1,9 +0,0 @@
-package com.github.constant;
-
-public class CommonConstant {
-    private CommonConstant() {
-    }
-
-    public static final int TOTAL_SIZE = 50_000;
-    public static final int CHUNK_SIZE = 5_000;
-}
